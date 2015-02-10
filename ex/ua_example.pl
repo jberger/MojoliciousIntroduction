44a638c70045ecb6b4da2575f5aa1b928f79a950
Joel Berger <joel.a.berger@gmail.com>
2015-02-10 17:17:15 -0600

    updated ua_example to remove proxy detection

--- ex/ua_example.pl
+++ ex/ua_example.pl
@@ -2,11 +2,9 @@ use Mojo::Base -strict;
 use Mojo::UserAgent;
 
 my $ua = Mojo::UserAgent->new(max_redirects => 10);
-$ua->detect_proxy;
 
 say $ua->get('api.metacpan.org/v0/release/Mojolicious')
        ->res->json->{version};
-
 
 use Mojo::URL;
 my $url = Mojo::URL->new('http://openlibrary.org/subjects/')
